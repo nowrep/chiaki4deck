a3351b718720c0de7c590e9ddc2617b6474e1469
David Rosca <nowrep@gmail.com>
2023-12-19 10:55:59 +0100

    QML Gui

--- gui/include/streamsession.h
+++ gui/include/streamsession.h
@@ -125,6 +125,7 @@ class StreamSession : public QObject
 		bool muted;
 		bool mic_connected;
 		bool allow_unmute;
+		bool input_blocked;
 
 		QHash<int, Controller *> controllers;
 #if CHIAKI_GUI_ENABLE_SETSU
@@ -227,6 +228,8 @@ class StreamSession : public QObject
 		void HandleMouseReleaseEvent(QMouseEvent *event);
 		void HandleMousePressEvent(QMouseEvent *event);
 		void HandleMouseMoveEvent(QMouseEvent *event, float width, float height);
+
+		void BlockInput(bool block) { input_blocked = block; }
 
 	signals:
 		void FfmpegFrameAvailable();
